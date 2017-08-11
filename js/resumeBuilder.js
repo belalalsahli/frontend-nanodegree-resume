7df73d6b62398889f80053a9bec4959db0ec4195
Belal Alsahli <belalalsahli@users.noreply.github.com>
2017-08-12 00:56:54 +0300

    Finished bio section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,4 @@
-
-
+// Bio section start
 var bio = {
     name: "Belal Alsahli",
     role: "Web developer!",
@@ -12,32 +11,64 @@ var bio = {
     },
     welcomeMessage: "Welcome!",
     skills: [
-        "skill 1", "skill 1", "skill 1"
+        "HTML5", "CSS3", "Java Script", "Sketch", "Photoshop"
     ],
-    biopic: "",
+    biopic: "..//images/fry.jpg",
     display: ""
 };
 
+var formattedName = HTMLheaderName.replace("%data%", bio["name"]);
+var formattedRole = HTMLheaderRole.replace("%data%", bio["role"]);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+var formattedbioPic = HTMLbioPic.replace("%data%", bio["biopic"]);
+var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio["welcomeMessage"]);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedLocation);
+$("#header").append(formattedbioPic);
+$("#header").append(formattedMessage);
 
-var education = {
-    "schools": {
-        "name": "Taibah university",
-        "location": "Medianh, Saudi Arabia",
-        "degree": "Bachelor degree",
-        "majors": [
-        "Computer science"
-        ],
-        "dates": "2011 - 2016",
-        "url": "https://www.taibahu.edu.sa"
-    },
-    "onlineCourses": [
-    {
-        "title": "Title of the course",
-        "school": "School"
-        "dates": "2017"
-        "url": "url!"
-    }
-    ]
+$("#header").append(HTMLskillsStart);
 
-//      display: function
-}
+bio.skills.forEach(function(skill){
+  var formattedSkill = HTMLskills.replace("%data%", skill);
+  $("#skills").append(formattedSkill);
+});
+
+// Bio section end
+
+//// education section start
+//var education = {
+//    "schools": {
+//        "name": "Taibah university",
+//        "location": "Medianh, Saudi Arabia",
+//        "degree": "Bachelor degree",
+//        "majors": ["Computer science"],
+//        "dates": "2011 - 2016",
+//        "url": "https://www.taibahu.edu.sa"
+//    },
+//    "onlineCourses": [
+//    {
+//        "title": "Title of the course",
+//        "school": "School"
+//        "dates": "2017"
+//        "url": "url!"
+//    }
+//]
+//
+////      display: function
+//}
+//
+//
+//
+//
+//
+//// education section end
