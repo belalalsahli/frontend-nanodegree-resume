ac08b8e4274db07c6df5eb4461682e4a2951d000
belalalsahli <belalalsahli@users.noreply.github.com>
2017-08-11 17:46:57 +0300

    Started coding
    Started coding

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,19 @@
-console.log("Belal did a change!");
-console.log("Belal did another change!");
+
+
+var bio = {
+    name: "Belal Alsahli",
+    role: "Web developer!",
+    contacts: {
+    mobile: "0569452081",
+    email: "belalalsahli@gmail.com",
+    github: "bilalalsehli",
+    twitter: "@belalalsahli",
+    location: "Riyadh, Saudi Arabia"
+    },
+    welcomeMessage: "Welcome!",
+    skills: [
+        "skill 1", "skill 1", "skill 1"
+    ],
+    biopic: "",
+    display: ""
+};
